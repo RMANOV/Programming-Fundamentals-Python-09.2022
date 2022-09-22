6ae99bd2104468094362485942f365a73d579ec2
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-09-22 18:35:27 +0300

    18.3

--- find_largest.py
+++ find_largest.py
@@ -2,11 +2,22 @@
 
 f_number = input()
 
-
 for i in range(len(f_number)):
-    print(len(f_number))
-    # if f_number[i] > f_number[i+1]:
-    #     print(f_number[i])
+    digits =int(f_number[i+1:])
+    last_digit = int(f_number[i])         
+    if last_digit > digits:
+        print(last_digit), print(digits)
+        break
+
+    # print(len(f_number))
+
+
+    # digital = int(f_number)
+    # print(digital)
+    # print(digital*2)
+
+    # if digital[i] > digital[i+1]:
+    #     print(digital[i])
     # else:
-    #     print(f_number[i+1])
+    #     print(digital[i+1])
     pass

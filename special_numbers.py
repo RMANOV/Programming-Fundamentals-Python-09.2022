515ef21d48aab8f250a28284b298fc282a6f7e73
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-09-24 16:13:26 +0300

    16.15

--- special_numbers.py
+++ special_numbers.py
@@ -2,14 +2,21 @@
 
 n = int(input())
 delim = "->"
-digit_list = []
+dlist = []
 text_list = [int(x) for x in str(n)]
 special_num = False
 
 for i in range(len(text_list)):
-    digit_list.__str__((text_list[i]))
-    sum_list = sum(digit_list)
-    print(sum_list, end=delim)
+    dlist.append(text_list[i])
+    if i < len(text_list) - 1:
+        dlist.append(delim)
+    
+print("".join(str(x) for x in dlist))
+
+# for i in range(len(text_list)):
+#     digit_list.__str__((text_list[i]))
+    # sum_list = sum(digit_list)
+    # print(sum_list, end=delim)
 
     # print(delim.join(str(x) for x in digit_list), end=" ")
     # if text_list[i] + text_list[i+1] == 7 or text_list[i] + text_list[i+1] == 11 or text_list[i] + text_list[i+1] == 5:
@@ -19,12 +26,6 @@ for i in range(len(text_list)):
     # else:
     #     special_num = False
     #     print(f"{text_list[i]}{delim}{special_num}")
-
-# print(int(digit_list))
-
-
-
-
 
 
 

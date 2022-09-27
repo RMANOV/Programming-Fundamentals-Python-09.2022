ee98639ca1b3ed6a21e6bb614f4943b94dfd02e0
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-09-27 18:08:08 +0300

    18

---
+++ exchange_integers.py
@@ -0,0 +1,21 @@
+
+
+first_number = int(input())
+second_number = int(input())
+
+c = first_number
+first_number = second_number
+second_number = c
+
+print(f"Before:\na = {second_number}\nb = {first_number}")
+print(f"After:\na = {first_number}\nb = {second_number}")
+
+
+
+
+
+
+
+
+# print(first_number)
+# print(second_number)

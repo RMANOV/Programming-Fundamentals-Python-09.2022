a828d0da8749be85abeea160d6d5629f78f271cd
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-09-27 19:33:07 +0300

    19.3

--- decrypting_messages.py
+++ decrypting_messages.py
@@ -2,14 +2,12 @@
 
 key_decryptor = int(input())
 number_lines = int(input())
-encrypted_message = input()
+decrypted_message = []
 
 
 for i in range(number_lines):
-    
-    decrypted_message = []
+    encrypted_message = input()
     for letter in encrypted_message:
         # decrypted_message += chr(ord(letter) + key_decryptor)
         decrypted_message.append(chr(ord(letter) + key_decryptor))
-
 print(*decrypted_message, sep="")

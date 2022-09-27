96c3f87c9323279214447824ccf318489c0e69be
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-09-27 19:50:51 +0300

    19.5

---
+++ balanced_brackets.py
@@ -0,0 +1,20 @@
+
+
+number_lines = int(input())
+brackets = []
+left_brackets = 0
+right_brackets = 0
+for i in range(number_lines):
+    brackets.append(input())
+
+
+for i in brackets:
+    if i == "(":
+        left_brackets += 1
+    if i == ")":
+        right_brackets += 1
+        
+if left_brackets == right_brackets:
+    print("BALANCED")
+else:
+    print("UNBALANCED")

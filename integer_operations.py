01a309d933951b20e64a21fedc8612ee6efbf10f
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-09-27 15:04:02 +0300

    15

---
+++ integer_operations.py
@@ -0,0 +1,9 @@
+
+
+first_number = int(input())
+second_number = int(input())
+thurth_number = int(input())
+forth_number = int(input())
+
+total = (first_number + second_number) // thurth_number * forth_number
+print(total)

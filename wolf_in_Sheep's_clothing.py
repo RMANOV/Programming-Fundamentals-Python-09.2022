43f4b82d9ec36b0f0a4b8b69df1040d8b10c1476
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-09-28 18:46:05 +0300

    18.45

--- wolf_in_Sheep's_clothing.py
+++ wolf_in_Sheep's_clothing.py
@@ -1,29 +1,15 @@
 
 
 
+from os import remove
+
+
 initial_string = input()
 initial_list = []
-initial_list.append((initial_string))
-initial_list.remove(' ' or '\t' or '\n')
-
-for i in initial_list:
-    if i == ' ' or '\t' or '\n':
-        initial_list.remove(i)
-
-
-    if i == 'wolf' and initial_list.index(i) == len(initial_list):
-        print("Please go away and stop eating my sheep")
-        print("Please go away and stop eating my sheep")
-        print("Please go away and stop eating my sheep")
-        break
-
-
-#how to get the index of the wolf in the list?
-
-
-
-# print(wolf_position+1)
-
+for i in initial_string:
+    initial_list.append(i)
+    
+print(initial_list)
 
 
 
@@ -35,17 +21,17 @@ for i in initial_list:
 
 
 
-# for j in range(len(initial_list)):
+for j in range(len(initial_list)):
     
 
 
-#     near = len(initial_list[j]) - len(initial_list)
-#     if near == 0 and initial_list[j] == "wolf":
-#         print("Please go away and stop eating my sheep")
-#         break
-#     else:
-#         print("Oi! Sheep number {sheep_position}! You are about to be eaten by a wolf!")
-#         break
+    near = len(initial_list[j]) - len(initial_list)
+    if initial_list[j] == "wolf":
+        print("Please go away and stop eating my sheep")
+        break
+    else:
+        print("Oi! Sheep number {sheep_position}! You are about to be eaten by a wolf!")
+        break
 
 
 

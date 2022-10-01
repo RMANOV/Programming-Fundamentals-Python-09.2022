8199efb1264f88096069bf34dbbda2ffe6c5430d
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-01 12:55:19 +0300

    13

--- list_statistics.py
+++ list_statistics.py
@@ -12,7 +12,7 @@ for i in range(number_of_lines):
     else:
         list_of_negative_numbers.append(number)
 
-
+print(list_of_positive_numbers)
+print(list_of_negative_numbers)
 print(f"Count of positives: {len(list_of_positive_numbers)}")
-
-print(f"Sum of negatives: {(list_of_negative_numbers).count()}")
+print(f"Sum of negatives: {sum(list_of_negative_numbers)}")

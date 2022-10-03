ac7d8d68635278e166ce8710510429089c4f8677
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-03 16:13:07 +0300

    16

--- numbers_filter.py
+++ numbers_filter.py
@@ -4,29 +4,28 @@ numbers_of_lines = int(input())
 
 initial_list = []
 
-for i in range(numbers_of_lines):
-    initial_string = input()
-    initial_list.append(initial_string)
-
 COMMAND_EVEN = "even"
 COMMAND_ODD = "odd"
 COMMAND_NEGATIVE = "negative"
 COMMAND_POSITIVE = "positive"
 
+for i in range(numbers_of_lines):
+    initial_string = int(input())
+    initial_list.append(initial_string)
+
 command = input()
 filtered_list = []
 
+
 for element in initial_list:
-
-    filtered_passes = (
-        (command == COMMAND_EVEN and element % 2 == 0) or \
-        (command == COMMAND_ODD and element % 2 != 0) or \
-        (command == COMMAND_NEGATIVE and element < 0) or \
-        (command == COMMAND_POSITIVE and element >= 0)
-    )
-
-    if filtered_passes:
+    filtered_pased = (command == COMMAND_EVEN and int(element) % 2 == 0) or \
+                        (command == COMMAND_ODD and int(element) % 2 != 0) or \
+                        (command == COMMAND_NEGATIVE and int(element) < 0) or \
+                        (command == COMMAND_POSITIVE and int(element) >= 0)
+    if filtered_pased:
         filtered_list.append(element)
+
+
 
 
 print(filtered_list)

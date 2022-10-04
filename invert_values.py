c4a2b9a7c79591b69d301720bb60ad684a1b2389
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-04 11:34:32 +0300

    11.3

--- invert_values.py
+++ invert_values.py
@@ -1,11 +1,13 @@
 
 
 inicial_string = input().split()
-inicial_list = []
-inicial_list.append(inicial_string)
+
+inicial_list = [int(i) for i in inicial_string]
+
+
 oposite_list = []
 
 for i in range(len(inicial_list)):
-    # inicial_list[i] = int(inicial_list[i])
-    oposite_list.append((inicial_list[i]) * -1)
+    oposite_list.append(inicial_list[i] * -1)
+    
 print(oposite_list)

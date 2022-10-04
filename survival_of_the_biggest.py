9a3f26fe2e9e798cc4fa11062e53c218ea90c8f6
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-04 18:16:01 +0300

    18.15

--- survival_of_the_biggest.py
+++ survival_of_the_biggest.py
@@ -8,10 +8,9 @@ final_list = []
 lowest_number = 0
 
 for i in range(len(initial_list)):
-    final_list.append(int(initial_list[i]))
-    lowest_number = min(final_list)
-    final_list.remove(lowest_number)
-    remove_count-=1
-    if remove_count == 0:
+    current_number = int(initial_list[i])
+    for j in range (remove_count+1):
+        final_list.append(initial_list[i][j])
+        lowest_number = min(final_list)
+        final_list[i].remove(lowest_number)
         print(final_list[i], end=", ")
-        break

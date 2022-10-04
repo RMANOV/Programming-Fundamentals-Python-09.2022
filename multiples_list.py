bb990e4856dfd8e61fb8efc00293aede3b79edc7
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-04 12:05:50 +0300

    12

--- multiples_list.py
+++ multiples_list.py
@@ -7,8 +7,12 @@ result_list.append(faktor)
 
 for i in range(1, count + 1):
     if i // faktor > 0 and i > 0:
-        result_list.append(i * faktor)
-    if result_list.count(faktor)>1:
+       result_list.append(i * faktor)
+    else:
+        continue
+if result_list.count(faktor)>1:
+    n=result_list.count(faktor)
+    for i in range(n-1):
         result_list.remove(faktor)
 result_list.sort()
 

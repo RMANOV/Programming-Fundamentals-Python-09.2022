83268d533539b09d6f5cbb53066ab62765ca5dfd
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-07 12:59:50 +0300

    13

--- messaging.py
+++ messaging.py
@@ -2,10 +2,28 @@
 
 number_list = input()
 number_list = [str(i) for i in number_list]
+index_list = []
+current_index = 0
 
 input_string = input()
 input_list = [input_string[i] for i in range(len(input_string))]
 
 output_list = []
+
+for i in range(number_list):
+    while number_list[i] != " ":
+        number_list(map(int, number_list[i]))
+        current_index += number_list[i]
+
+
+
+
+
+
+
+
+
+
+
 
 print(number_list)

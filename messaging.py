e0fc99c08e89b0ce687917230a2e156d85c71983
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-07 12:42:07 +0300

    12.4

---
+++ messaging.py
@@ -0,0 +1,11 @@
+
+
+number_list = input()
+number_list = [str(i) for i in number_list]
+
+input_string = input()
+input_list = [input_string[i] for i in range(len(input_string))]
+
+output_list = []
+
+print(number_list)

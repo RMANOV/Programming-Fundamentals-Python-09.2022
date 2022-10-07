99ecf9c2a96299f6b855ca288cfe0b5574c8c336
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-07 11:20:08 +0300

    11.2

--- faro_shuffle.py
+++ faro_shuffle.py
@@ -1,16 +1,14 @@
-card_string = input().split()
-card_list = []
-card_list.append(card_string)
 
+
+deck_of_cards = input().split()
 number_shuffles = int(input())
 
 for shuffle in range(number_shuffles):
-    final_deck = []
-    midle_of_deck = int(len(card_list[0]) // 2)
-    left_part = card_list[0:midle_of_deck]
-    right_part = card_list[midle_of_deck::]
-    for i in range(len(left_part)):
-        final_deck.append(left_part[i])
-        final_deck.append(right_part[i])
-    card_list = final_deck
-    print(final_deck)
+    left_deck = deck_of_cards[:len(deck_of_cards) // 2]
+    right_deck = deck_of_cards[len(deck_of_cards) // 2:]
+    deck_of_cards = []
+    for i in range(len(left_deck)):
+        deck_of_cards.append(left_deck[i])
+        deck_of_cards.append(right_deck[i])
+
+print(deck_of_cards)

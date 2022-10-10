f62bf4a6a892e348ee34df1bf6d8b41990e3e9dd
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-10 10:53:18 +0300

    10.5

---
+++ repeat_string.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+
+def repeat_string(string, times):
+    return string * times
+
+initial_string = input()
+number_of_repeats = int(input())
+print(repeat_string(initial_string, number_of_repeats))

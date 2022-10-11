1024f19060a28578e4f86b3d530e37bbeb512f6e
Ruslan Manov <96174405+RMANOV@users.noreply.github.com>
2022-10-11 22:03:22 +0300

    22

--- password_validator.py
+++ password_validator.py
@@ -1,29 +1,30 @@
 
 
-def password_validator(password):
+def password_validator_len(password):
     """Checks if password is valid"""
     if 6 > len(password) < 10:
         print("Password must be between 6 and 10 characters")
-        # return False
-    # if password.islower():
-    #     return False
-    # if password.isupper():
-    #     return False
-    if password.isdigit() < 2:
+        return False
+def password_validator_digit(password1):
+    if password1.isdigit() < 2:
         print("Password must have at least 2 digits")
-        # return False
-    if password.isalpha():
+        return False
+def password_validator_alfa(password2):
+    if password2.isalpha():
         print("Password must consist only letters and digits")
-        # return False
-    else:
-        return("Password is valid")
+        return False
+
+# return("Password is valid")
         # return True
     # print("Password is valid")
     # return True
 
-
-
-
-
 pass_word = input()
-print(password_validator(pass_word))
+if password_validator_len(pass_word) == False:
+    password_validator_len(pass_word)
+elif password_validator_digit(pass_word) == False:
+    password_validator_digit(pass_word)
+elif password_validator_alfa(pass_word) == False:
+    password_validator_alfa(pass_word)
+else:
+    print("Password is valid")

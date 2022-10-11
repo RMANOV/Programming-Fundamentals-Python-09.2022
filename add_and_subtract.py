079eb4dbcd8063cea91584775fbdab2577e13824
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-11 14:52:16 +0300

    14.5

---
+++ add_and_subtract.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+def add_and_subtract(a, b, c):
+    """Returns the result of adding two numbers and then subtracting the third"""
+    return a + b - c
+
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+
+
+print(add_and_subtract(first_number, second_number, third_number))

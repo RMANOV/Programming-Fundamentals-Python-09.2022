218716a90b63cb97f9a437cc6244ae5a9433de28
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-12 13:18:24 +0300

    13.2

--- password_validator.py
+++ password_validator.py
@@ -1,30 +1,32 @@
 
-def password_validator(password):
-        
+def password_validator_len(password):
     if len(password) < 6 or len(password) > 10:
-        print("Password must be between 6 and 10 characters long.")
-        # return False
+        return False
+    return True
+def passord_validator_digit(password):
+    if not any(char.isdigit() for char in password):
+        return False
+    return True
 
     # if not any(char.isupper() for char in password):
     #     return False
     # if not any(char.islower() for char in password):
     #     return False
-
-    if (char.isalpha() for char in password):
-        print("Password must consist only of letters and digits")
-        # return False
-
-    if (char.isdigit() for char in password):
-        print("Password must have at least 2 digits")
-        # return False
-
-    if password_validator(password):
-        return True
-    else:
+def passord_validator_special(password):
+    if not any(char in "!@#$%^&*()_+-=" for char in password):
         return False
+    return True
 
 
 
-password = input()
-if password_validator(password):
-    print("Valid password.")
+password = input().strip()
+
+if password_validator_len(password) and passord_validator_digit(password) and passord_validator_special(password):
+    print("Valid")
+else:
+    if not password_validator_len(password):
+        print("Password must be between 6 and 10 characters")
+    if not passord_validator_digit(password):
+        print("Password must have at least 2 digits")
+    if not passord_validator_special(password):
+        print("Password must consist only of letters and digits")

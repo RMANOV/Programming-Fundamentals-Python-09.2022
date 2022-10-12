55468a824e8e47c80469256b31842a245207b28d
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-12 17:50:58 +0300

    17.5

--- messaging.py
+++ messaging.py
@@ -1,24 +1,12 @@
 
 
+number_list = [int(i) for i in input().split()]
+digit_list = []
+
+for i in number_list:
+    if i not in digit_list:
+        digit_list.append(int(i))
 
 
 
-number_list = input()
-
-index_list = []
-current_index = 0
-
-input_string = input()
-input_list = [input_string[i] for i in range(len(input_string))]
-
-output_list = []
-
-for digit in number_list:
-   while not digit == " " in number_list[digit]:
-       current_index += number_list[digit]
-       index_list.append(current_index)
-
-
-
-
-print(current_index)
+print(digit_list)

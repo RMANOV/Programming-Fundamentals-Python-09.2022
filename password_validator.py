322bb8be205b654d7a4a57152ccb655543f0e778
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-12 12:00:32 +0300

    12

--- password_validator.py
+++ password_validator.py
@@ -8,24 +8,26 @@ def password_validator(password):
     if len(password) < 6 and len(password) > 10:
         print("Password must be between 6 and 10 characters long.")
         return False
+        
     # if not any(char.isupper() for char in password):
     #     return False
     # if not any(char.islower() for char in password):
     #     return False
-    if (char.isdigit()<2 for char in password):
-        print("Password must contain at least two digits.")
-        return False
+
     if (char.isalpha()<2 for char in password):
-        print("Password must contain at least two letters.")
+        print("Password must consist only of letters and digits")
         return False
-    # return True
+
+    if (char.isdigit()<2 for char in password):
+        print("Password must have at least 2 digits")
+        return False
+
+    return True
 
 
 
 
 
 password = input("Enter a password: ")
-if not password_validator(password):
-    password_validator(password)
-else:
+if password_validator(password):
     print("Valid password.")

bd0b7fb02de8290dc61deebe2293b6f6d6927459
Ruslan Manov <96174405+RMANOV@users.noreply.github.com>
2022-10-17 21:25:06 +0300

    21.25

--- to-do_list.py
+++ to-do_list.py
@@ -9,7 +9,8 @@ while True :
     task = command[1]
     task_list.append((priority, task))
     task_list.sort()
-    # remove the priority from the list with list comprehension
-    task_list = [task for task in task_list if priority.isdigit() == True task_list.remove(priority)]
+    
+    
 
-    print(task_list)
+
+print(task_list)

e8c14a4cd4658e2833f6128df0d2fc5cc13a6df0
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-18 19:04:54 +0300

    19.05

---
+++ next_version.py
@@ -0,0 +1,12 @@
+
+
+initial_version = input().split(".")
+initial_version = [int(x) for x in initial_version]
+initial_version[2] += 1
+if initial_version[2] > 9:
+    initial_version[2] = 0
+    initial_version[1] += 1
+    if initial_version[1] > 9:
+        initial_version[1] = 0
+        initial_version[0] += 1
+print(".".join([str(x) for x in initial_version]))

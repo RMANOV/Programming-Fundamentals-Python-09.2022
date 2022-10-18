c86229a083f7882211fc737f8bd96caf6370ab45
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-18 18:05:50 +0300

    18

--- the_office.py
+++ the_office.py
@@ -6,9 +6,8 @@ factor = int(input())
 employe_happiness = [x * factor for x in employe_happiness]
 average_happiness = sum(employe_happiness) / len(employe_happiness)
 happy_employees = [x for x in employe_happiness if x >= average_happiness]
+
 if len(happy_employees) >= len(employe_happiness) / 2:
     print(f"Score: {len(happy_employees)}/{len(employe_happiness)}. Employees are happy!")
 else:
     print(f"Score: {len(happy_employees)}/{len(employe_happiness)}. Employees are not happy!")
-
-    

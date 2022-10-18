89217adf3d4d3c264327981fdabcd6d9401c3c6c
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-18 18:58:19 +0300

    19.01

--- which_are_in.py
+++ which_are_in.py
@@ -7,7 +7,7 @@ list_of_matched_part_of_words = []
 
 # result = [x for x in first_list if x in second_list]
 
-list_of_matched_part_of_words = [x for x in first_list for y in second_list if x in y]
+list_of_matched_part_of_words = [x for x in first_list if any(x in y for y in second_list)]
 # substrings = [first for first in first_sequence if any(first in second for second in second_sequence)]
 
 # for first in first_list:

9092a8e0f580d8204b93ba27d32a600abe31620c
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-18 16:05:44 +0300

    16

--- sorting_names.py
+++ sorting_names.py
@@ -1,8 +1,5 @@
 
 
-initial_list = [initial_list for initial_list in input().split(", ")]
-
-sorted_to_length = sorted(initial_list, key=lambda x: len(x), reverse=True)
-sorted_alphabetically = sorted(sorted_to_length, key=lambda x: x)
-
-print(sorted_alphabetically)
+names = input().split(', ')
+sorted_names = sorted(names, key=lambda item: (-len(item), item))
+print(sorted_names)

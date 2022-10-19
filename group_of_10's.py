2cd2668db9646e191835243ac3826da0db87722d
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-19 12:19:28 +0300

    12.2

--- group_of_10's.py
+++ group_of_10's.py
@@ -14,27 +14,19 @@ list90 = [x for x in initial_list_sorted if 80 < x <= 90]
 list100 = [x for x in initial_list_sorted if 90 < x <= 100]
 
 
-for i in initial_list_sorted:
-    if i<10:
-        list10.append(i)
-        print(f"Group of 10's: {i}")
-    # elif i<20:
-    #     print(f"Group of 20's: {i}")
-    # elif i<30:
-    #     print(f"Group of 30's: {i}")
-    # elif i<40:
-    #     print(f"Group of 40's: {i}")
-    # elif i<50:
-    #     print(f"Group of 50's: {i}")
-    # elif i<60:
-    #     print(f"Group of 60's: {i}")
-    # elif i<70:
-    #     print(f"Group of 70's: {i}")
-    # elif i<80:
-    #     print(f"Group of 80's: {i}")
-    # elif i<90:
-    #     print(f"Group of 90's: {i}")
-    # elif i<100:
-    #     print(f"Group of 100's: {i}")
+print(f"Group of 10's: {list10}")
+print(f"Group of 20's: {list20}")
+print(f"Group of 30's: {list30}")
+print(f"Group of 40's: {list40}")
+print(f"Group of 50's: {list50}")
 
-        
+if len(list60) > 0:
+    print(f"Group of 60's: {list60}")
+if len(list70) > 0:
+    print(f"Group of 70's: {list70}")
+if len(list80) > 0:
+    print(f"Group of 80's: {list80}")
+if len(list90) > 0:
+    print(f"Group of 90's: {list90}")
+if len(list100) > 0:
+    print(f"Group of 100's: {list100}")

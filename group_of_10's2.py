8389811773c19219b47df76c9453201fce29def7
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-19 13:22:13 +0300

    13.25

--- group_of_10's2.py
+++ group_of_10's2.py
@@ -1,6 +1,16 @@
+from re import X
+
+
 initial_list_unsorted = [int(i) for i in input().split(", ")]
 
 for n in range(1,11):
     list = [x for x in initial_list_unsorted if 10*(n-1) < x <= 10*n]
-    if len(list) > 0:
-        print(f"Group of {10*n}'s: {list}")
+    initial_list_unsorted = [x for x in initial_list_unsorted if x not in list]
+    if n<11 and len(list)>=0:
+        print(f"Group of {n*10}'s: {list}")
+    if initial_list_unsorted == []:
+        break
+
+
+ 
+

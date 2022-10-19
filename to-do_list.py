44b9e6ec4d7eeec11b7f99dd1f4f6b7d5d83f273
Ruslan Manov <96174405+RMANOV@users.noreply.github.com>
2022-10-19 19:17:53 +0300

    19.20

--- to-do_list.py
+++ to-do_list.py
@@ -4,10 +4,4 @@ while True :
     command = input()
     if command == "End":
         break
-    command = command.split("-")
-    prioryty = int(command[1])
-    task = command[0]
-    task_list.append((prioryty, task))
-    task_list = [task_list[i] for i in range(len(task_list)) if i % 2 == 0]
-
-print(task_list)
+    task_list = command.split("-")

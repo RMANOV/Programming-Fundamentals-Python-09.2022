3726abac4b2706505c4ecc505ae56d7bd10c48b6
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-22 12:26:02 +0300

    12.25

--- decipher_this!.py
+++ decipher_this!.py
@@ -6,28 +6,30 @@ secret_message_list = input().split()
 #convert int to letter whit chr()
 #insert the letter in a first list
 #remove the first three numbers from the item
-def get_secret_message(secret_message_list):
-    secret_message = [x for x in secret_message_list]
-    for item in secret_message_list:
-        first_number = item[0]
-        second_number = item[1]
-        third_number = item[2]
-        numbers = first_number + second_number + third_number
-        numbers = int(numbers)
-        letter = chr(numbers)
-        secret_message.insert(0, letter)
-        # item = item[3:]
-        print(secret_message)
-    return secret_message
+
+secret_message = [x for x in secret_message_list]
+number = []
+letter = []
+for item in secret_message:
+    for i in word:
+        if i.isdigit():
+            number.append(i)
+        else:
+            letter.append(i)
+number = int(''.join(number))
+letter = chr(number)
+secret_message.insert(0, letter)
+secret_message.remove(number)
+print
 
 # switch the second and last characters of each wordv
 # get the item of above list and switch the second and last characters
-def switch_characters(secret_message):
-    for item in secret_message:
-        item[0],item[-1]=item[-1],item[0]
-    return secret_message
+# def switch_characters(get_secret_message):
+#     for item in secret_message:
+#         item[0],item[-1]=item[-1],item[0]
+#     return secret_message
 
 
 # print the result
-def print_result(secret_message):
-    print("".join(switch_characters(get_secret_message(secret_message_list))))
+# def print_result(switch_characters):
+#     print("".join(switch_characters(get_secret_message(secret_message_list))))
---
+++ decipher_this!2.py
@@ -0,0 +1,48 @@
+secret_message_list = input().split()
+
+
+# Get item by item from the list
+# check first, second and third number of the item, store them in a list, concatenate them and convert to int
+# convert int to letter whit chr()
+# insert the letter in a first list
+# remove the first three numbers from the item
+
+secret_message = [x for x in secret_message_list]
+number = []
+letter = []
+for item in secret_message:
+    for i in item:
+        item_list = list(item)
+        for i in range(3):
+            if item_list[i].isdigit():
+                number.append(item_list[i])
+
+            number = int("".join(number))
+            letter.append(chr(number))
+            del item_list[0:3]
+            item = "".join(item_list)
+            number = []
+            letter = []
+            break
+
+        # if i.isdigit():
+        #     number.append(i)
+        #     item = item.replace(i, '')
+        #     number = int(''.join(number))
+        #     letter.append(chr(number))
+        #     item[i] = letter
+        #     number = []
+        #     letter = []
+print(secret_message)
+
+# switch the second and last characters of each wordv
+# get the item of above list and switch the second and last characters
+# def switch_characters(get_secret_message):
+#     for item in secret_message:
+#         item[0],item[-1]=item[-1],item[0]
+#     return secret_message
+
+
+# print the result
+# def print_result(switch_characters):
+#     print("".join(switch_characters(get_secret_message(secret_message_list))))

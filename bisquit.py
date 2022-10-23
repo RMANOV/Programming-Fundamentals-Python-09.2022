d48a1ae6eff72d6bf97f9a0832656e8a2114cf00
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-23 10:08:59 +0300

    10.08

--- bisquit.py
+++ bisquit.py
@@ -11,11 +11,46 @@ bisquits_per_month = 0
 while True:
     days += 1
     if days % 3 == 0:
-        bisquits_per_day -= bisquits_per_day * 0.75
-    
+        bisquits_per_month += bisquits_per_day * 0.75
+    bisquits_per_month += bisquits_per_day
     if days == 30:
         break
 
-print(f"You have produced {bisquits_per_day} biscuits for the past month.")
+    my_factory_vs_competitor = bisquits_per_month / number_of_bisquits_of_the_competitor_for_30_days * 100
+
+print(f"You have produced {bisquits_per_month:.0f} biscuits for the past month.")
+
+if my_factory_vs_competitor > 100:
+    print(f"You produce {my_factory_vs_competitor:.2f} percent more biscuits.")
+else:
+    print(f"You produce {my_factory_vs_competitor:.2f} percent less biscuits.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#     days += 1
+#     if days % 3 == 0:
+#         bisquits_per_day -= bisquits_per_day * 0.75
+    
+#     if days == 30:
+#         break
+
+# print(f"You have produced {bisquits_per_day} biscuits for the past month.")
 
     

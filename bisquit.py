8d1ceff17fa3c2ba4f175527b500a1afd8e38338
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-23 11:37:43 +0300

    11.4

--- bisquit.py
+++ bisquit.py
@@ -14,10 +14,12 @@ bisquits_per_month = (bisquits_per_day * days1) + (bisquits_per_day * days2 * 0.
 
 
 my_factory_vs_competitor = (bisquits_per_month / number_of_bisquits_of_the_competitor_for_30_days * 100)-100
+my_factory_vs_competitor_rounded = my_factory_vs_competitor.__round__(2)
+
 
 print(f"You have produced {bisquits_per_month:.0f} biscuits for the past month.")
 
 if my_factory_vs_competitor > 1:
-    print(f"You produce {my_factory_vs_competitor:.2f} percent more biscuits.")
+    print(f"You produce {my_factory_vs_competitor_rounded:.2f} percent more biscuits.")
 else:
-    print(f"You produce {abs(my_factory_vs_competitor):.2f} percent less biscuits.")
+    print(f"You produce {abs(my_factory_vs_competitor_rounded):.2f} percent less biscuits.")

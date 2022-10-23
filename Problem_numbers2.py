2d8367110c7092a2192ce676b49ef968913c171c
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-23 10:36:33 +0300

    10.4

---
+++ Problem_numbers2.py
@@ -0,0 +1,22 @@
+
+
+initial_list = [int(i) for i in input().split()]
+
+while True:
+    command = input().split()
+    if command[0] == "Finish":
+        break
+    elif command[0] == "Add":
+        initial_list.append(int(command[1]))
+    elif command[0] == "Remove":
+        initial_list.remove(int(command[1]))
+    elif command[0] == "Replace":
+        if int(command[1]) in initial_list:
+            initial_list[initial_list.index(int(command[1]))] = int(command[2])
+    elif command[0] == "Collapse":
+        for i in initial_list:
+            if i < int(command[1]):
+                initial_list.remove(i)
+
+print(*initial_list)
+  

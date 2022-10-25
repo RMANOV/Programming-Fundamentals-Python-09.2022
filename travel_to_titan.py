be8c95d8768d7d5e6976ca4607fb5844137d0ec6
Ruslan Manov <96174405+RMANOV@users.noreply.github.com>
2022-10-25 23:00:25 +0300

    23

--- travel_to_titan.py
+++ travel_to_titan.py
@@ -15,40 +15,79 @@ while True:
         print("You have reached Titan!, all passengers are safe.")
         break
     command = command.split("||")
-    if command[0] == "Add":
-        if command[1] == "Fuel":
-            fuel += int(command[2])
-        elif command[1] == "Ammunition":
-            ammunition += int(command[2])
-    elif command[0] == "Move":
-        if command[1] == "Nuke":
-            if ammunition >= 1:
-                distance_in_light_years += int(command[2])
-                ammunition -= 1
-            else:
-                print("Not enough ammunition to perform this trip!")
-        elif command[1] == "Faster":
-            if fuel >= int(command[2]):
-                distance_in_light_years += int(command[2])
-                fuel -= int(command[2])
-            else:
-                print("Not enough fuel to perform this trip!")
-    elif command[0] == "Travel":
-        if command[1] == "Normal":
-            if fuel >= int(command[2]):
-                distance_in_light_years += int(command[2])
-                fuel -= int(command[2])
-            else:
-                print("Not enough fuel to perform this trip!")
-        elif command[1] == "Time":
-            if fuel >= int(command[2]) * 1.5:
-                distance_in_light_years += int(command[2])
-                fuel -= int(command[2]) * 1.5
-            else:
-                print("Not enough fuel to perform this trip!")
-    elif command[0] == "Refuel":
-        fuel += int(command[1])
-    elif command[0] == "Status":
-        print(f"Total Distance: {distance_in_light_years}")
-        print(f"Total Fuel: {fuel}")
-        print(f"Total Ammunition: {ammunition}")
+    if command[0] == "Travel":
+        distance_in_light_years += int(command[1])
+        fuel += int(command[2])
+        ammunition += int(command[3])
+        if fuel < distance_in_light_years
+            print("Mission failed.")
+            break
+            
+        else:
+            print(f'The spaceship traveled {distance_in_light_years} light years.')
+            break
+    elif command[0] == "Fuel":
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #     if command[1] == "Fuel":
+    #         fuel += int(command[2])
+    #     elif command[1] == "Ammunition":
+    #         ammunition += int(command[2])
+    # elif command[0] == "Move":
+    #     if command[1] == "Nuke":
+    #         if ammunition >= 1:
+    #             distance_in_light_years += int(command[2])
+    #             ammunition -= 1
+    #         else:
+    #             print("Not enough ammunition to perform this trip!")
+    #     elif command[1] == "Faster":
+    #         if fuel >= int(command[2]):
+    #             distance_in_light_years += int(command[2])
+    #             fuel -= int(command[2])
+    #         else:
+    #             print("Not enough fuel to perform this trip!")
+    # elif command[0] == "Travel":
+    #     if command[1] == "Normal":
+    #         if fuel >= int(command[2]):
+    #             distance_in_light_years += int(command[2])
+    #             fuel -= int(command[2])
+    #         else:
+    #             print("Not enough fuel to perform this trip!")
+    #     elif command[1] == "Time":
+    #         if fuel >= int(command[2]) * 1.5:
+    #             distance_in_light_years += int(command[2])
+    #             fuel -= int(command[2]) * 1.5
+    #         else:
+    #             print("Not enough fuel to perform this trip!")
+    # elif command[0] == "Refuel":
+    #     fuel += int(command[1])
+    # elif command[0] == "Status":
+    #     print(f"Total Distance: {distance_in_light_years}")
+    #     print(f"Total Fuel: {fuel}")
+    #     print(f"Total Ammunition: {ammunition}")

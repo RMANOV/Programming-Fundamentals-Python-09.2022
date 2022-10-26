ab197205191e75219e2cc27a91924894c93d8fb1
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-10-26 18:24:10 +0300

    18.3

--- orders2.py
+++ orders2.py
@@ -1,21 +1,80 @@
 
 
 number_of_orders = int(input())
-current_order = []
-orders = []
-for i in range(number_of_orders+1):
-    for i in range(3):
-        if not i == 0 or i < 0:
-            current_order.append((input()))
-        else:
-            continue
-    current_cost = float(current_order[0]) * int(current_order[1]) * int(current_order[2])
-    orders.append(current_cost)
-    print(f"The price for the coffee is: ${current_cost}")
-    current_order = []
+total = 0
+current_cost = 0
 
-total_orders = sum(orders)
-print("Total: ${total_orders:.2f}")
+for n in range(number_of_orders):
+    price = float(input())
+    days = int(input())
+    quantity = int(input())    
+    if price<0 or days<0 or quantity<0:
+        continue
+    current_cost = price * quantity * days
+    if current_cost <=0:
+        continue
+    
+    print(f"The price for the coffee is: ${current_cost:.2f}")
+    total += current_cost
+    current_cost = 0
+
+print(f"Total: ${total:.2f}")
+# print(f"The average cost of the coffee is: ${total/days:.2f}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# current_order = []
+# orders = []
+# for i in range(number_of_orders+1):
+#     for i in range(3):
+#         if not i == 0 or i < 0:
+#             current_order.append((input()))
+#         else:
+#             continue
+#     current_cost = float(current_order[0]) * int(current_order[1]) * int(current_order[2])
+#     orders.append(current_cost)
+#     print(f"The price for the coffee is: ${current_cost}")
+#     current_order = []
+
+# total_orders = sum(orders)
+# print("Total: ${total_orders:.2f}")
 
 
 

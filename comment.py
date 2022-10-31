cb0f51f6307bc16d3398d7d9ba2a67fc5d3a4128
Ruslan Manov <96174405+RMANOV@users.noreply.github.com>
2022-10-31 20:57:12 +0200

    21

--- comment.py
+++ comment.py
@@ -5,3 +5,9 @@ class Comment:
         self.username = username
         self.content = content
         self.likes = likes
+
+
+comment = Comment("user1", "I like this book")
+print(comment.username)
+print(comment.content)
+print(comment.likes)

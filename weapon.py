5d99dd3dfbfba443b75c0e311609d572ac650aba
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-01 17:40:29 +0200

    17.40

--- weapon.py
+++ weapon.py
@@ -1,2 +1,18 @@
 
 
+class Weapon:
+    bullets = 0
+
+    def __init__(self, bullets):
+        self.bullets = bullets
+
+    def shoot(self):
+        if self.bullets > 0:
+            self.bullets -= 1
+            return f'shooting...'
+        else:
+            return f'no bullets left'
+
+    def __repr__(self):
+        return f'Remaining bullets: {self.bullets}'
+

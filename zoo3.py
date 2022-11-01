ef3d361906e9882a7c64b2cf9b5520491a8c0568
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-01 15:16:23 +0200

    15.15

--- zoo3.py
+++ zoo3.py
@@ -12,14 +12,61 @@ class Zoo:
         self.fishes = []
         self.birds = []
 
-    def add_animal(self, species):
-        Zoo.__animals += 1
+    def add_animal(self, species, name):
+
+        if species == 'mammal':
+
+            self.mamals.append(name)
+            Zoo.__animals += 1
+
+        elif species == 'fish':
+
+            self.fishes.append(name)
+            Zoo.__animals += 1
+
+        elif species == 'bird':
+
+            self.birds.append(name)
+            Zoo.__animals += 1
 
     def get_info(self, species):
-        return f'{species} in {self.name_of_zoo}: {Zoo.__animals}'
+            
+            if species == 'mammal':
+    
+                return f'Mammals in {self.name_of_zoo}: {", ".join(self.mamals)}'
+    
+            elif species == 'fish':
+    
+                return f'Fishes in {self.name_of_zoo}: {", ".join(self.fishes)}'
+    
+            elif species == 'bird':
+    
+                return f'Birds in {self.name_of_zoo}: {", ".join(self.birds)}'
 
     def get_total(self):
+        
         return f'Total animals: {Zoo.__animals}'
+
+zoo_name = input()
+zoo = Zoo(zoo_name)
+
+n = int(input())
+
+for _ in range(n):
+    
+        species, name = input().split()
+        zoo.add_animal(species, name)
+
+species = input()
+print(zoo.get_info(species))
+print(zoo.get_total())
+
+
+
+
+
+
+
 
 
 

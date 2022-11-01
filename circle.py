bffe44b5e00c3cf1729f58b501abac0ae516b4e0
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-01 15:53:31 +0200

    15.55

---
+++ circle.py
@@ -0,0 +1,27 @@
+
+
+class Circle:
+
+    __pi = 3.14
+
+    def __init__(self, diameter):
+        self.diameter = diameter
+
+    def calculate_circumference(self):
+        return self.diameter * Circle.__pi
+
+    def calculate_area(self):
+        return (self.diameter / 2) ** 2 * Circle.__pi
+
+    def calculate_area_of_sector(self, angle):
+        return (angle / 360) * Circle.calculate_area(self)
+
+diameter = int(input())
+angle = int(input())
+
+circle = Circle(diameter)
+print(f'{circle.calculate_circumference():.2f}')
+print(f'{circle.calculate_area():.2f}')
+print(f'{circle.calculate_area_of_sector(angle):.2f}')
+
+# Path: circle.py

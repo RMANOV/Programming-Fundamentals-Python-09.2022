3a2d13e936e1628a6a652a0a95702e2f736bc351
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-01 18:51:12 +0200

    18.52

--- town.py
+++ town.py
@@ -7,11 +7,12 @@ class Town:
         self.latitude = latitude
         self.longitude = longitude
 
-        def set_latitude(self, latitude):
-            self.latitude = latitude
+    def set_latitude(self, latitude):
+        self.latitude = latitude
 
-        def set_longitude(self, longitude):
-            self.longitude = longitude
+    def set_longitude(self, longitude):
+        self.longitude = longitude
 
-        def __repr__(self):
-            return f'Town: {self.name} | Latitude: {self.latitude} | Longitude: {self.longitude}'
+    def __repr__(self):
+        return f'Town: {self.name} | Latitude: {self.latitude} | Longitude: {self.longitude}'
+

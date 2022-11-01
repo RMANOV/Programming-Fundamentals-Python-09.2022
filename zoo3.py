37887a5dec7f80672e04a651351c1275cb3d8d6d
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-01 15:41:45 +0200

    15.42

--- zoo3.py
+++ zoo3.py
@@ -29,22 +29,15 @@ class Zoo:
             self.birds.append(name)
             Zoo.__animals += 1
 
-    def get_info(self, species):
-            
-            if species == 'mammal':
-    
-                return f'Mammals in {self.name_of_zoo}: {", ".join(self.mamals)}'
-    
-            elif species == 'fish':
-    
-                return f'Fishes in {self.name_of_zoo}: {", ".join(self.fishes)}'
-    
-            elif species == 'bird':
-    
+    def get_info(self, species):            
+            if species == 'mammal':    
+                return f'Mammals in {self.name_of_zoo}: {", ".join(self.mamals)}'    
+            elif species == 'fish':    
+                return f'Fishes in {self.name_of_zoo}: {", ".join(self.fishes)}'    
+            elif species == 'bird':    
                 return f'Birds in {self.name_of_zoo}: {", ".join(self.birds)}'
 
-    def get_total(self):
-        
+    def get_total(self):        
         return f'Total animals: {Zoo.__animals}'
 
 zoo_name = input()
@@ -52,8 +45,7 @@ zoo = Zoo(zoo_name)
 
 n = int(input())
 
-for _ in range(n):
-    
+for _ in range(n):    
         species, name = input().split()
         zoo.add_animal(species, name)
 

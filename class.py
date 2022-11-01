c2fd186a8f9df5da372562e127bc0e47797c60ec
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-01 19:09:56 +0200

    19.10

---
+++ class.py
@@ -0,0 +1,30 @@
+
+
+class Class:
+    __students_count = 22
+    students = []
+    grades = []
+
+    def __init__(self, name):
+        self.name = name
+
+    def add_student(self, name: str, grade: float):
+        self.students.append(name)
+        self.grades.append(grade)
+
+    def get_average_grade(self):
+        return sum(self.grades) / len(self.grades)
+
+    def __repr__(self):
+        result = f'Class name: {self.name}\n'
+
+        for i in range(len(self.students)):
+            result += f'{self.students[i]} - {self.grades[i]}\n'
+
+        result += f'The students in {self.name}: {self.__students_count}. Average grade: {self.get_average_grade():.2f}'
+        return result
+
+
+
+
+    

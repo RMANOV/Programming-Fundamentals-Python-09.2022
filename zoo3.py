d4e682df16c3891e46153b7efd79a9e3ff19942d
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-01 12:02:25 +0200

    12.02

--- zoo3.py
+++ zoo3.py
@@ -3,12 +3,14 @@
 class Zoo:
 
     __animals = 0
-    mamals = []
-    fishes = []
-    birds = []
+
 
     def __init__(self, name_of_zoo):
+
         self.name_of_zoo = name_of_zoo
+        self.mamals = []
+        self.fishes = []
+        self.birds = []
 
     def add_animal(self, species):
         Zoo.__animals += 1
@@ -19,11 +21,6 @@ class Zoo:
     def get_total(self):
         return f'Total animals: {Zoo.__animals}'
 
-    def add_mammal(self, mammal):
-        Zoo.mamals.append(mammal)
-
-    def get_mammals(self):
-        
 
 
 
@@ -34,20 +31,5 @@ class Zoo:
 
 
 
-
-zoo_name = input()
-
-zoo = Zoo(zoo_name)
-
-while True:
-    command = input()
-    if command == 'End':
-        break
-
-    species = command
-    zoo.add_animal(species)
-
-print(zoo.get_info(species))
-print(zoo.get_total())
 
 # Path: zoo3.py

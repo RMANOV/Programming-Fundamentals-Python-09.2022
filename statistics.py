aae539560b34268ad90e56125fadd85a302853e0
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-06 14:37:32 +0200

    14.38

--- statistics.py
+++ statistics.py
@@ -1,20 +1,42 @@
 
 
-bakery = {input().split(" ")[0]: int(input()) for _ in range(int(input()))}
+bakery = {}
 
-while command!= "statistics":
-
-    command = input().split(": ")
-    if command[0] not in bakery:
-        keys = command[0]
-        values = int(command[1])
-        bakery[keys] = values
+command = input().split(": ")
+while command[0] != "statistics":
+    
+    if command[0] in bakery:
+        bakery[command[0]] += int(command[1])
     else:
-        bakery[keys] += values
+        bakery[command[0]] = int(command[1])
+        
+        command = input().split(": ")
         
 print("Products in stock:")
-for key, value in bakery.items():
-    print(f"- {key}: {value}")
-
+print(*[f"- {key}: {value}" for key, value in bakery.items()])
+      
 print(f"Total Products: {len(bakery)}")
 print(f"Total Quantity: {sum(bakery.values())}")
+
+      
+        
+        
+
+    
+    
+        
+#     if command[0] not in bakery:
+#         keys = command[0]
+#         values = int(command[1])
+#         bakery[keys] = values
+#     else:
+#         bakery[keys] += values
+        
+#     command = input().split(": ")
+    
+# print("Products in stock:")
+# for key, value in bakery.items():
+#     print(f"- {key}: {value}")
+
+# print(f"Total Products: {len(bakery)}")
+# print(f"Total Quantity: {sum(bakery.values())}")

1f1f866b60f84e2d613b7345b0749813dba69f7a
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-08 17:27:44 +0200

    17.27

---
+++ A Miner Task.py
@@ -0,0 +1,31 @@
+
+
+resources = {}
+command = input()
+
+
+while not command == "stop":
+    current_resource = command
+    if command.isdigit():
+        quantity = int(command)
+        resources[current_resource] += quantity
+    else:
+        if current_resource not in resources:
+            resources[current_resource] = 0
+    command = input()
+    
+for resource, quantity in resources.items():
+    print(f"{resource} -> {quantity}")
+    
+    
+    
+    # resource = command[0]
+    # quantity = int(command[1])
+    # if resource not in resources:
+    #     resources[resource] = 0
+    # resources[resource] += quantity
+    # command = input()
+    
+# for key, value in resources.items():
+#     print(f"{key} -> {value}")
+# print(f'{"".join([f"{key} -> {value})\n" for key, value in resources.items()])}')

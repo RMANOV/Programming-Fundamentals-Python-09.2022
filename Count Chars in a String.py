071e56c10d32efb124dd58a6489270aaa223b9fa
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-08 16:47:17 +0200

    16.47

--- Count Chars in a String.py
+++ Count Chars in a String.py
@@ -11,20 +11,25 @@
 #     print(f"{char} -> {occurrences}")
 
 #create a dictionary to store the count of each character
-initial_string = input()
+initial_list = input().split()
+symbols = ''.join(initial_list)
+letters = {}
 
-dictionary = {}
-for i in initial_string:
-    if i in dictionary:
-        dictionary[i] += 1
-    else:
-        dictionary[i] = 1
+letters = {char: symbols.count(char) for char in symbols}
+for char, occurrences in letters.items():
+    print(f"{char} -> {occurrences}")
 
-# Check if the keys are alfanumeric and print the count of each character
-dictionary = {k: v for k, v in dictionary.items() if k.isalnum()}
-# print(f'{key} -> {value}' for key, value in dictionary.items())
-for keys, values in dictionary.items():
-    print(f'{keys} -> {values}')
+# for i in initial_string:
+#     if i in dictionary:
+#         dictionary[i] += 1
+#     else:
+#         dictionary[i] = 1
+
+# # Check if the keys are alfanumeric and print the count of each character
+# # dictionary = {k: v for k, v in dictionary.items() if k.isalnum()}
+# # print(f'{key} -> {value}' for key, value in dictionary.items())
+# for keys, values in dictionary.items():
+#     print(f'{keys} -> {values}')
 
 # print(key + " -> " + str(value) for key, value in dictionary.items())
 # for key, value in dictionary.items():

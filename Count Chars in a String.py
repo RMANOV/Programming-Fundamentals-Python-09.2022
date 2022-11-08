2af7b44ae78f28de40c7f6a91a45cdedf6454954
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-08 16:31:11 +0200

    16.3

--- Count Chars in a String.py
+++ Count Chars in a String.py
@@ -1,8 +1,17 @@
 
 
-initial_string = input()
+# current_input = input().split()
+# symbols = ''.join(current_input)
+# letters = {}
+# for symbol in symbols:
+#     if symbol not in letters.keys():
+#         letters[symbol] = 0
+#     letters[symbol] += 1
+# for char, occurrences in letters.items():
+#     print(f"{char} -> {occurrences}")
 
 #create a dictionary to store the count of each character
+initial_string = input()
 
 dictionary = {}
 for i in initial_string:
@@ -10,13 +19,14 @@ for i in initial_string:
         dictionary[i] += 1
     else:
         dictionary[i] = 1
-        
 
 # Check if the keys are alfanumeric and print the count of each character
 dictionary = {k: v for k, v in dictionary.items() if k.isalnum()}
-# print(key + " -> " + str(value) for key, value in dictionary.items())
-print(f'{key} -> {value}' for key, value in dictionary.items())
+# print(f'{key} -> {value}' for key, value in dictionary.items())
+for keys, values in dictionary.items():
+    print(f'{keys} -> {values}')
 
+# print(key + " -> " + str(value) for key, value in dictionary.items())
 # for key, value in dictionary.items():
 #         print(key + " -> " + str(value))
     # print(i + " -> " + str(dictionary[i]))

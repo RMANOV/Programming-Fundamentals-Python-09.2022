d46eaed3e6fe2435295b64ea9bde892dc188befb
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-08 10:46:07 +0200

    10.45

--- ASCII_values.py
+++ ASCII_values.py
@@ -1,5 +1,12 @@
 
 
 initial_list = [input().split(",")]
-ascii_dict = {initial_list[0][i]: ord(initial_list[0][i]) for i in range(len(initial_list[0]))}
+ascii_dict = {}
+
+for i in range(len(initial_list[0])):
+    ascii_dict[initial_list[0][i]] = ord(initial_list[0][i])
+    
 print(ascii_dict)
+
+# ascii_dict = {initial_list[0][i]: ord(initial_list[0][i]) for i in range(len(initial_list[0]))}
+# print(ascii_dict)

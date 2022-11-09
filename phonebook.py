d05ec6dabf6ebf79b8595231a294b0fc2c302006
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-09 16:27:28 +0200

    16.27

--- phonebook.py
+++ phonebook.py
@@ -1,23 +1,51 @@
 
 
 phonebook = {}
-command = []
 command = input().split('-')
-count_of_searched_names = 0
-while len(command) != 1:
-    command = [x for x in input().split('-') if not x.isdigit()]
-    phonebook = {x: int(y) for x, y in zip(command[::2], command[1::2])}
-    
-    if len(command) == 1 and command[0].isdigit():
-        count_of_searched_names = int(command[0])
-        break
 
-for _ in range(count_of_searched_names):
-    searched_name = input()
-    if searched_name in phonebook:
-        print(f'{searched_name} -> {phonebook[searched_name]}')
+while not len(command) == 1:
+    name = command[0]
+    number = command[1]
+    phonebook[name] = number
+    command = input().split('-')
+    
+for i in range(int(command[0])):
+    name = input()
+    if name in phonebook:
+        print(f'{name} -> {phonebook[name]}')
     else:
-        print(f'Contact {searched_name} does not exist.')
+        print(f'Contact {name} does not exist.')
+        
+# Path: phonebook.py
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# count_of_searched_names = 0
+# while len(command) != 1:
+#     command = [x for x in input().split('-') if not x.isdigit()]
+#     phonebook = {x: int(y) for x, y in zip(command[::2], command[1::2])}
+    
+#     if len(command) == 1 and command[0].isdigit():
+#         count_of_searched_names = int(command[0])
+#         break
+
+# for _ in range(count_of_searched_names):
+#     searched_name = input()
+#     if searched_name in phonebook:
+#         print(f'{searched_name} -> {phonebook[searched_name]}')
+#     else:
+#         print(f'Contact {searched_name} does not exist.')
 
 
 # command = input().split('-')

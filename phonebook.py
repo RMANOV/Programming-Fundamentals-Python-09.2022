715138fae24cbfe3b01a125381fbc6d5edf1c851
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-09 15:37:43 +0200

    15.37

--- phonebook.py
+++ phonebook.py
@@ -1,26 +1,44 @@
 
 
 phonebook = {}
+command = []
+
 command = input().split('-')
+phonebook[command[0]] = command[1]
 
-while not len(command) == 1:
+def add_contact():
+    command = input().split('-')
     phonebook[command[0]] = command[1]
-    command = input().split('-')
+    return phonebook
 
-    if len(command) ==1:
-        break
-    
-    command = input().split('-')
-
-number_searched_names = int(command[0])
-command = input()
-
-for _ in range(number_searched_names):
-    name = input()
-    if name in phonebook:
-        print(f'{name} -> {phonebook[name]}')
+def search_contact():
+    command = input()
+    if command in phonebook:
+        print(f'{command} -> {phonebook[command]}')
     else:
-        print(f'Contact {name} does not exist.')
+        print(f'Contact {command} does not exist.')
+        
+
+
+# phonebook[command[0]] = command[1]
+# while not len(command) == 1:
+    
+#     command = input().split('-')
+#     phonebook[command[0]] = command[1]
+#     if len(command) ==1:
+#         break
+    
+#     command = input().split('-')
+
+# number_searched_names = int(command[0])
+# command = input()
+
+# for _ in range(number_searched_names):
+#     name = input()
+#     if name in phonebook:
+#         print(f'{name} -> {phonebook[name]}')
+#     else:
+#         print(f'Contact {name} does not exist.')
         
 # Path: phonebook.py
         

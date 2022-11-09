80b5cb26a340ad6eff7bb4eed93d178278e1b7f4
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-09 16:06:35 +0200

    16.06

--- phonebook.py
+++ phonebook.py
@@ -2,19 +2,38 @@
 
 phonebook = {}
 command = []
-
 command = input().split('-')
-phonebook[command[0]] = command[1]
-
-while len(command) == 1:
-    if len(command) == 1:
+count_of_searched_names = 0
+while len(command) != 1:
+    command = [x for x in input().split('-') if not x.isdigit()]
+    phonebook = {x: int(y) for x, y in zip(command[::2], command[1::2])}
+    
+    if len(command) == 1 and command[0].isdigit():
         count_of_searched_names = int(command[0])
-        for i in range(count_of_searched_names):
-            name = input()
-            if name in phonebook:
-                print(f'{name} -> {phonebook[name]}')
-            else:
-                print('Contact {name} does not exist.')
+        break
+
+for _ in range(count_of_searched_names):
+    searched_name = input()
+    if searched_name in phonebook:
+        print(f'{searched_name} -> {phonebook[searched_name]}')
+    else:
+        print(f'Contact {searched_name} does not exist.')
+
+
+# command = input().split('-')
+# phonebook[command[0]] = command[1]
+
+
+
+# while len(command) == 1:
+#     if len(command) == 1:
+#         count_of_searched_names = int(command[0])
+#         for i in range(count_of_searched_names):
+#             name = input()
+#             if name in phonebook:
+#                 print(f'{name} -> {phonebook[name]}')
+#             else:
+#                 print('Contact {name} does not exist.')
             
 
 

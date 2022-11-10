184a627200efbb2c5517363a48b2da05a2edc9b8
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-10 19:34:11 +0200

    19.34

--- student_academy.py
+++ student_academy.py
@@ -4,29 +4,23 @@ number_of_commands = int(input())
 students = {}
 student_name = 0
 student_grade = 0
+command_list = []
 
-
-for i in range(number_of_commands):
+for i in range(number_of_commands*2):
     command = input()
-    if i % 2 == 0:
-        student_name = command
-        if student_name not in students:
-            students[student_name] = []
-        else:
-            command = input()
-            student_grade = float(command)
-            students[student_name].append(student_grade)
+    command_list.append(command)
+
+for command in range(0, len(command_list), 2):
+    student_name = command_list[command]
+    student_grade = float(command_list[command+1])
+    if student_name not in students:
+        students[student_name] = [student_grade]
     else:
-        if float(command.isdigit()):
-            student_grade = float(command)
-            students[student_name].append(student_grade)
-        else:
-            student_name = command
-            student_grade = float(command)
-            students[student_name].append(student_grade)
+        students[student_name].append(student_grade)
+
 
 for student, grades in students.items():
-    if len(grades) > 1:
+    if len(grades) >= 1:
         average_grade = sum(grades) / len(grades)
         if average_grade >= 4.50:
             print(f'{student} -> {average_grade:.2f}')

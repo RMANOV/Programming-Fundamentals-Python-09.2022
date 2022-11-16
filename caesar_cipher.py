55e0fa3e3313c13395fb922fc4eb56b9ac962f2b
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-16 15:51:50 +0200

    15.52

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -3,6 +3,9 @@
 initial_string = input()
 final_string = ""
 
+# final_string = enumerate(chr(ord(initial_string[i]) + 3) for i in range(len(initial_string)))
+
+
 for i in range(len(initial_string)):
     final_string += chr(ord(initial_string[i]) + 3)
 

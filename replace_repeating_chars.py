c6813141f6e358da3e28309ced3e1c8d2dcfe81a
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-16 16:25:35 +0200

    16.26

--- replace_repeating_chars.py
+++ replace_repeating_chars.py
@@ -2,10 +2,30 @@
 
 initial_string = input()
 final_string = ''
-if initial_string[0] == initial_string[1]:
-    final_string += initial_string[0]
+
 for i in range(len(initial_string)):
-    if initial_string[i] != initial_string[i-1]:
+    if i == 0:
+        final_string += initial_string[i]
+    elif initial_string[i] != initial_string[i-1]:
         final_string += initial_string[i]
 
 print(final_string)
+
+
+
+
+
+
+
+
+
+
+
+
+# if initial_string[0] == initial_string[1]:
+#     final_string += initial_string[0]
+# for i in range(len(initial_string)):
+#     if initial_string[i] != initial_string[i-1]:
+#         final_string += initial_string[i]
+
+# print(final_string)

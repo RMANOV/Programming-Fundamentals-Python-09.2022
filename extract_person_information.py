9e9b6741e2e746ba56a542c50bda3573ee384cc7
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-17 14:43:35 +0200

    14.43

--- extract_person_information.py
+++ extract_person_information.py
@@ -8,9 +8,13 @@ for i in range(number_of_persons):
     person_data = input().split()
     for j in range(len(person_data)):
         if person_data[j].startswith('@'):
-            name = person_data[j].replace('@', '').replace('|', '')
+            # get all characters after @ and delete all characters after the |
+            name = person_data[j][1:].split('|')[0]            
+            # name = person_data[j].replace('@', '').replace('|', '')
         elif person_data[j].startswith('#'):
-            age = person_data[j].replace('#', '').replace('*', '')
+            # get all characters after # and delete all characters after the *
+            age = person_data[j][1:].split('*')[0]
+            # age = person_data[j].replace('#', '').replace('*', '')
     print(f'{name} is {age} years old.')
     name = ''
     age = ''

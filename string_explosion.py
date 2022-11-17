1ccef45cc745155dbf3bfde221bfdde7ba322a2d
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-17 10:35:40 +0200

    10.35

--- string_explosion.py
+++ string_explosion.py
@@ -1,32 +1,29 @@
 
 
 initial_string = input()
-final_string = ""
+string_list = list(initial_string)
 explosive_power = 0
 
-for char in initial_string:
-    if explosive_power > 0 and char != ">":
+for i in range(len(string_list)):
+    if string_list[i] == ">":
+        explosive_power += int(string_list[i + 1])
+    elif explosive_power > 0:
+        string_list[i] = ""
         explosive_power -= 1
-        continue
-    elif char == ">":
-        explosive_power += int(initial_string[initial_string.index(char) + 1])
-    final_string += char
-print(final_string)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+string_list = [x for x in string_list if x != ""]
+print("".join(string_list))
+
+
+# for char in initial_string:
+#     if explosive_power > 0 and char != ">":
+#         explosive_power -= 1
+#         continue
+#     elif char == ">":
+#         explosive_power += int(initial_string[initial_string.index(char) + 1])
+#     final_string += char
+# print(final_string)
+
 #     if char != ">" and explosive_power > 0:
 #         explosive_power -= 1
 #     elif char == ">":
@@ -36,16 +33,7 @@ print(final_string)
 #         final_string += char
 # print(final_string)
      
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
 #     if char == ">":
 #         final_string += char
 #         explosive_power += int(initial_string[initial_string.index(char) + 1])
@@ -56,10 +44,6 @@ print(final_string)
 #         final_string += char
         
 # print(final_string)
-
-
-
-
 
 # for char in range(len(initial_string)):
 #     if initial_string[char] != '>' and explosive_power > 0:

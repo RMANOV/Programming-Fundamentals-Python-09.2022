12b31741e502c75820bedad1bb475014911c4ec5
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-17 10:30:32 +0200

    10.30

--- string_explosion.py
+++ string_explosion.py
@@ -5,14 +5,36 @@ final_string = ""
 explosive_power = 0
 
 for char in initial_string:
-    if char != ">" and explosive_power > 0:
+    if explosive_power > 0 and char != ">":
         explosive_power -= 1
+        continue
     elif char == ">":
         explosive_power += int(initial_string[initial_string.index(char) + 1])
-        final_string += char
-    else:
-        final_string += char
+    final_string += char
 print(final_string)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     if char != ">" and explosive_power > 0:
+#         explosive_power -= 1
+#     elif char == ">":
+#         explosive_power += int(initial_string[initial_string.index(char) + 1])
+#         final_string += char
+#     else:
+#         final_string += char
+# print(final_string)
      
     
     

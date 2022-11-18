8e0522a19f0cc4c207a7835293fcac85ec829d13
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-18 11:36:42 +0200

    11.37

---
+++ morse_code_translator.py
@@ -0,0 +1,14 @@
+
+
+massage = input().split('|')
+decryptor = {A: ".-"; B: "-..."; C: "-.-."; D: "-.."; E: "."; F: "..-."; G: "--."; H: "...."; I: ".."; J: ".---"; K: "-.-"; L: ".-.."; M: "--"; N: "-."; O: "---"; P: ".--."; Q: "--.-"; R: ".-."; S: "..."; T: "-"; U: "..-"; V: "...-"; W: ".--"; X: "-..-"; Y: "-.--"; Z: "--.."; 0: "-----"; 1: ".----"; 2: "..---"; 3: "...--"; 4: "....-"; 5: "....."; 6: "-...."; 7: "--..."; 8: "---.."; 9: "----."}
+decrypted_word = ''
+decrypted_massage = ''
+for word in massage:
+    for letter in word:
+        decrypted_word += decryptor[letter]
+        decrypted_massage += decrypted_word + ' '
+        decrypted_word = ''
+print(decrypted_massage)
+
+# Path: morse_code_translator.py

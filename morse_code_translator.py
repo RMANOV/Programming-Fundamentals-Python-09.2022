fb12d5e4be920828263593b6bd179cd9738fe484
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-18 11:55:19 +0200

    11.56

--- morse_code_translator.py
+++ morse_code_translator.py
@@ -4,10 +4,11 @@ massage = input().split('|')
 decryptor = { '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y', '--..': 'Z', '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9' }
 decrypted_word = ''
 decrypted_massage = ''
+
 for word in massage:
-    for letter in word:
-        if decryptor.get(letter) != None:
-            decrypted_word += decryptor.get(letter)
+    for letter in word.split():
+        decrypted_word += decryptor[letter]
     decrypted_massage += decrypted_word + ' '
     decrypted_word = ''
+    
 print(decrypted_massage)

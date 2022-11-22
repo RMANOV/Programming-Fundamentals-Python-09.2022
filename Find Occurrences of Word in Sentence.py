fea65dd1eb8a9ab23a6e75d51906b42795bc9ec6
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-22 17:20:08 +0200

    17.2

---
+++ Find Occurrences of Word in Sentence.py
@@ -0,0 +1,19 @@
+
+
+import re
+
+strings = input()
+target_word = input()
+repeat_count = 0
+# Write a program that finds how many times a word is used in a string.
+# The output is a single number indicating the number of times the string contains the word. 
+# Note that letter case does not matter – it is case-insensitive.
+
+
+search_pattern = r'\b' + target_word + r'\b'
+result = re.finditer(search_pattern, strings)
+
+for word in result:
+    repeat_count += 1
+
+print(repeat_count)

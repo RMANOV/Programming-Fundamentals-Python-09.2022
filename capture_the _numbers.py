435031989003201a008b0077aa80544f43651fe4
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-22 14:19:18 +0200

    14.20

--- capture_the _numbers.py
+++ capture_the _numbers.py
@@ -5,7 +5,11 @@ import re
 strings = input()
 
 # Capture only the numbers in the string
-
-search_pattern = r'\d+'
-result = re.findall(search_pattern, strings)
-print(' '.join(result))
+while True:
+    search_pattern = r'\d+'
+    result = re.findall(search_pattern, strings)
+    if result:
+        print(''.join(result), end=' ')
+        strings = strings.replace(''.join(result), '', 1)
+    else:
+        break

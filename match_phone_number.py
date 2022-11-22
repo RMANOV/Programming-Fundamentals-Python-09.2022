9a0b7afc491fef85342131901314e24a665323e0
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-22 10:05:23 +0200

    10.05

--- match_phone_number.py
+++ match_phone_number.py
@@ -12,6 +12,7 @@ phones = input()
 # o	The number consists of 7 digits (separated into two groups of 3 and 4 digits, respectively). 
 # •	The different parts are separated by either a space or a hyphen ('-').
 
-search_pattern = r'\+359([ ]) 2 \d{3} \d{4}\b|\+359-2-\d{3}-\d{4}\b'
+# search_pattern = r'\+359\S2\b\s\d{3}\s\d{4}\b|\+359-2-\d{3}-\d{4}\b'
+search_pattern = r'\+359( |-)2\b( |-)\d{3}( |-)\d{4}\b'
 result = re.findall(search_pattern, phones)
 print(', '.join(result))

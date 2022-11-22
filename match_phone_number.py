950521259e1592b6506898571a581cdd6760bd4c
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-22 10:07:14 +0200

    10.07

--- match_phone_number.py
+++ match_phone_number.py
@@ -14,5 +14,9 @@ phones = input()
 
 # search_pattern = r'\+359\S2\b\s\d{3}\s\d{4}\b|\+359-2-\d{3}-\d{4}\b'
 search_pattern = r'\+359( |-)2\b( |-)\d{3}( |-)\d{4}\b'
-result = re.findall(search_pattern, phones)
-print(', '.join(result))
+# result = re.findall(search_pattern, phones)
+# print(', '.join(result))
+result = re.finditer(search_pattern, phones)
+for match in result:
+    print(match.group(0), end=' ')
+

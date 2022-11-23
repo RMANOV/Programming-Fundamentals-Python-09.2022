de37f36bfa69e1d1c62c51df93f8248ea7ee12e4
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-23 14:20:56 +0200

    14.20

---
+++ computer_store.py
@@ -0,0 +1,44 @@
+
+
+# Write a program that prints you a receipt for your new computer. 
+# You will receive the parts' prices (without tax) until you receive what type of customer this is - special or regular. 
+# Once you receive the type of customer you should print the receipt.
+# The taxes are 20% of each part's price you receive. 
+# If the customer is special, he has a 10% discount on the total price with taxes.
+# If a given price is not a positive number, you should print "Invalid price!" on the console and continue with the next price.
+# If the total price is equal to zero, you should print "Invalid order!" on the console.
+
+# •	The receipt should be in the following format: 
+# "Congratulations you've just bought a new computer!
+# Price without taxes: {total price without taxes}$
+# Taxes: {total amount of taxes}$
+# -----------
+# Total price: {total price with taxes}$"
+# Note: All prices should be displayed to the second digit after the decimal point! 
+# The discount is applied only on the total price. Discount is only applicable to the final price!
+
+def receipt():
+    total_price = 0
+    while True:
+        price = float(input())
+        if price < 0:
+            print("Invalid price!")
+            continue
+        elif price == 0:
+            break
+        total_price += price
+    customer_type = input()
+    taxes = total_price * 0.2
+    total_price += taxes
+    if customer_type == "special":
+        total_price *= 0.9
+    if total_price == 0:
+        print("Invalid order!")
+    else:
+        print("Congratulations you've just bought a new computer!")
+        print(f"Price without taxes: {total_price - taxes:.2f}$")
+        print(f"Taxes: {taxes:.2f}$")
+        print("-----------")
+        print(f"Total price: {total_price:.2f}$")
+
+result = receipt()

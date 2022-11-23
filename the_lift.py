8792f909f3d36b23781c55a7d96f0545714b3e31
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-23 15:19:46 +0200

    15.20

--- the_lift.py
+++ the_lift.py
@@ -22,13 +22,13 @@ wagons = [int(x) for x in input().split()]
 
 for i in range(len(wagons)):
     while wagons[i] < 4:
-        if numbers_of_people == 0:
+        if numbers_of_people <= 0:
             break
-        if wagons[i] == 4:
+        if wagons[i] >= 4:
             break
         wagons[i] += 1
         numbers_of_people -= 1
-    
+
 if numbers_of_people == 0:
     print("The lift has empty spots!")
     print(*wagons)

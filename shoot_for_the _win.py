200b9cf5be06be49fb5fb02f10638de49ec5bfc3
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-24 18:54:47 +0200

    19

--- shoot_for_the _win.py
+++ shoot_for_the _win.py
@@ -4,7 +4,8 @@
 # You will receive a sequence with integers, separated by a single space, representing targets and their value. 
 # Afterward, you will be receiving indices until the "End" command is given, and you need to print the targets and the count of shot targets.
 # Every time you receive an index, you need to shoot the target on that index, if it is possible. 
-# Every time you shoot a target, its value becomes -1, and it is considered a shot. Along with that, you also need to:
+# Every time you shoot a target, its value becomes -1, and it is considered a shot. 
+# Along with that, you also need to:
 # •	Reduce all the other targets, which have greater values than your current target, with its value. 
 # •	Increase all the other targets, which have less than or equal value to the shot target, with its value.
 # Keep in mind that you can't shoot a target, which is already shot. You also can't increase or reduce a target, which is considered a shot.
@@ -16,6 +17,10 @@
 # Output
 # Shot targets 3 -> -1 -1 130 -1
 # Shot targets: 4 -> -1 120 -1 66 -1 -1
+
+
+
+# target_list = [x for x in 
 
 
 targets_list1 = input().split()
@@ -31,11 +36,21 @@ current_state = 0
 shots_number = 0
 
 while command != 'End':
+    if command > len(targets_list):
+        continue
     current_state = targets_list[command]
     targets_list[command] = -1
     shots_number += 1
     for i in range(len(targets_list)):
-        if targets_list[i] !=targets_list[command] and targets_list[i] > current_state:
+        if targets_list[i] == targets_list[command]:
+            continue
+        if targets_list[i] > current_state:
+            targets_list[i] -= current_state
+            current_state = 0
+        else:
+            targets_list[i] += current_state
+            current_state = 0
+            
            
     current_state = 0
-    command = input()
+    command = int(input())

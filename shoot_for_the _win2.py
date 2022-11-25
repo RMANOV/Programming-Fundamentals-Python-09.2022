752a10927e47add48ccce0feca755d171f229e3a
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-25 18:32:34 +0200

    18.33

---
+++ shoot_for_the _win2.py
@@ -0,0 +1,28 @@
+# Write a program that helps you keep track of your shot targets.
+# You will receive a sequence with integers, separated by a single space, representing targets and their value.
+# Afterward, you will be receiving indices until the "End" command is given, and you need to print the targets and the count of shot targets.
+# Every time you receive an index, you need to shoot the target on that index, if it is possible.
+# Every time you shoot a target, its value becomes -1, and it is considered a shot.
+# Along with that, you also need to:
+# •	Reduce all the other targets, which have greater values than your current target, with its value.
+# •	Increase all the other targets, which have less than or equal value to the shot target, with its value.
+# Keep in mind that you can't shoot a target, which is already shot. You also can't increase or reduce a target, which is considered a shot.
+# When you receive the "End" command, print the targets in their current state and the count of shot targets in the following format:
+# "Shot targets: {count} -> {target1} {target2}… {targetn}"
+# Input / Constraints
+# •	On the first line of input, you will receive a sequence of integers, separated by a single space – the target sequence.
+# •	On the following lines, until the "End" command, you be receiving integers each on a single line – the index of the target to be shot.
+# Output
+# Shot targets 3 -> -1 -1 130 -1
+# Shot targets: 4 -> -1 120 -1 66 -1 -1
+
+
+target_list = [int(x) for x in input().split() if int(x) != ""]
+shots_number = 0
+current_state = 0
+
+target_index = input()
+
+while target_index != 'End':
+    if target_index != 'End':
+        

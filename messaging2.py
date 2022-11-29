b1042145250ac1ddae12f089d3a0b8c0b9623b4e
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-29 18:43:39 +0200

    18.45

--- messaging2.py
+++ messaging2.py
@@ -9,39 +9,56 @@
 # When you find a char, you should add it to the message and remove it from the string. 
 # It means that for the following index, the text will be with one character less.
 
-def get_sum_of_digits(number):
+initial_sequence = [int(i) for i in input().split()]
+initial_text = input()
+
+for i in initial_sequence:
     current_sum = 0
-    while number > 0:
-        current_sum += number % 10 
-        number = number // 10 # integer division
-    return current_sum
+    while i > 0:
+        current_sum += i % 10
+        i = i // 10
+        
+        if current_sum > len(initial_text):
+            current_sum -= len(initial_text)
+            
+    print(initial_text[current_sum], end="")
+    initial_text = initial_text[:current_sum] + initial_text[current_sum + 1:]
 
-def get_index(number_list, messaging):
-    digit_list = []
-    for i in number_list:
-        current_sum = 0
-        while i > 0:
-            current_sum += i % 10
-            i = i // 10
-            digit_list.append(current_sum)
-    return digit_list
 
-def get_message(digit_list, messaging):
-    message = ""
-    for i in digit_list:
-        if len(messaging) <= 0:
-            i=0
-        if i >= len(messaging):
-            i = i % len(messaging)
-        message += messaging[i]
-        messaging = messaging[i + 1:]
-    return message
 
-def main():
-    number_list = [int(i) for i in input().split()]
-    messaging = input()
-    digit_list = get_index(number_list, messaging)
-    message = get_message(digit_list, messaging)
-    print(message)
+# def get_sum_of_digits(number):
+#     current_sum = 0
+#     while number > 0:
+#         current_sum += number % 10 
+#         number = number // 10 # integer division
+#     return current_sum
 
-main()
+# def get_index(number_list, messaging):
+#     digit_list = []
+#     for i in number_list:
+#         current_sum = 0
+#         while i > 0:
+#             current_sum += i % 10
+#             i = i // 10
+#             digit_list.append(current_sum)
+#     return digit_list
+
+# def get_message(digit_list, messaging):
+#     message = ""
+#     for i in digit_list:
+#         if len(messaging) <= 0:
+#             i=0
+#         if i >= len(messaging):
+#             i = i % len(messaging)
+#         message += messaging[i]
+#         messaging = messaging[i + 1:]
+#     return message
+
+# def main():
+#     number_list = [int(i) for i in input().split()]
+#     messaging = input()
+#     digit_list = get_index(number_list, messaging)
+#     message = get_message(digit_list, messaging)
+#     print(message)
+
+# main()

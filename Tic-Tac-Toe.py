de46228de39d143bbd4061ccee17d20b0b63e055
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-30 15:31:12 +0200

    15.31

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -9,6 +9,50 @@
 # If the second player wins, print "Second player won". 
 # Otherwise, print "Draw!".
 
+def accpet_input():
+    field = []
+    for _ in range(3):
+        field.append(input().split())
+    return field
+
+def check_for_winner(field):
+    # check for horizontal winner
+    for row in field:
+        if row.count(row[0]) == len(row) and row[0] != "0":
+            return row[0]
+    # check for vertical winner
+    for col in range(len(field)):
+        check = []
+        for row in field:
+            check.append(row[col])
+        if check.count(check[0]) == len(check) and check[0] != "0":
+            return check[0]
+    # check for diagonal winner
+    diagonals = []
+    for ix in range(len(field)):
+        diagonals.append(field[ix][ix])
+    if diagonals.count(diagonals[0]) == len(diagonals) and diagonals[0] != "0":
+        return diagonals[0]
+    # check for second diagonal winner
+    second_diagonals = []
+    for col in range(len(field)):
+        second_diagonals.append(field[col][len(field) - 1 - col])
+    if second_diagonals.count(second_diagonals[0]) == len(second_diagonals) and second_diagonals[0] != "0":
+        return second_diagonals[0]
+    return "Draw!"
+
+main_field = accpet_input()
+winner = check_for_winner(main_field)
+if winner == "1":
+    print("First player won")
+elif winner == "2":
+    print("Second player won")
+else:
+    print("Draw!")
+
+
+
+
 # def main():
 #     field = []
 #     for _ in range(3):
@@ -40,6 +84,7 @@
 #         print('Second player won')
 #         return
 #     print('Draw!')
+# main()
 
 
 # first_line = input().split()

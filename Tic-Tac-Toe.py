33dc8ac38acc5e54a679eec8c02998bb10e0bca9
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-11-30 15:23:49 +0200

    15.25

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -9,16 +9,49 @@
 # If the second player wins, print "Second player won". 
 # Otherwise, print "Draw!".
 
-first_line = input().split()
-second_line = input().split()
-third_line = input().split()
+# def main():
+#     field = []
+#     for _ in range(3):
+#         field.append(input().split())
+#     for row in range(3):
+#         if field[row][0] == field[row][1] == field[row][2] == '1':
+#             print('First player won')
+#             return
+#         if field[row][0] == field[row][1] == field[row][2] == '2':
+#             print('Second player won')
+#             return
+#     for col in range(3):
+#         if field[0][col] == field[1][col] == field[2][col] == '1':
+#             print('First player won')
+#             return
+#         if field[0][col] == field[1][col] == field[2][col] == '2':
+#             print('Second player won')
+#             return
+#     if field[0][0] == field[1][1] == field[2][2] == '1':
+#         print('First player won')
+#         return
+#     if field[0][0] == field[1][1] == field[2][2] == '2':
+#         print('Second player won')
+#         return
+#     if field[0][2] == field[1][1] == field[2][0] == '1':
+#         print('First player won')
+#         return
+#     if field[0][2] == field[1][1] == field[2][0] == '2':
+#         print('Second player won')
+#         return
+#     print('Draw!')
 
-field = [first_line, second_line, third_line]
-for row in field:
-    if row[0] == row[1] == row[2] == '1' or row[2] == row[1] == row[0] == '1':
-        exit()
-    elif row[0] == row[1] == row[2] == '2' or row[2] == row[1] == row[0] == '2':
-        print('Second player won')
-        exit()
-    else:
-        print('Draw!')
+
+# first_line = input().split()
+# second_line = input().split()
+# third_line = input().split()
+
+# field = [first_line, second_line, third_line]
+# for row in field:
+#     if row[0] == row[1] == row[2] == '1' or row[2] == row[1] == row[0] == '1':
+#         exit()
+#     elif row[0] == row[1] == row[2] == '2' or row[2] == row[1] == row[0] == '2':
+#         print('Second player won')
+#         exit()
+#     else:
+#         print('Draw!')

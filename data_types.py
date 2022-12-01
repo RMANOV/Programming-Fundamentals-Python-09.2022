3c0163333360eeca83176e805535c7b754175ed5
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-12-01 17:49:43 +0200

    17.51

--- data_types.py
+++ data_types.py
@@ -6,39 +6,77 @@
 # •	If the data type is a string, surround the input with "$".
 # Print the result on the console.
 
-def get_data_type():
-# How to get the data type?
-    num = input()
-    if num.isdigit():
-        return "int"
-    elif num.replace(".", "").isdigit():
-        return "real"
-    elif num.isalpha():
-        return "string"
-    else:
-        return "unknown"
-
-def multiply_by_two(num):
+def multiply_by_2(num):
     return num * 2
 
-def multiply_by_one_and_half(num):
-    return num * 1.5
+def multiply_by_1_5(num):
+    return f"{num * 1.5:.2f}"
 
-def surround_with_dollar_sign(num):
-    return "$" + num + "$"
+def surround_with_dollar_sign(string):
+    return f"${string}$"
 
-def print_result(result):
-    print(result)
+def aquire_input():
+    return input()
 
 def main():
-    data_type = get_data_type()
-    num = input()
+    data_type = input()
     if data_type == "int":
-        result = multiply_by_two(int(num))
+        print(multiply_by_2(int(aquire_input())))
     elif data_type == "real":
-        result = multiply_by_one_and_half(float(num))
+        print(multiply_by_1_5(float(aquire_input())))
     elif data_type == "string":
-        result = surround_with_dollar_sign(num)
-    print_result(result)
+        print(surround_with_dollar_sign(aquire_input()))
 
 main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def get_data_type():
+#     if 
+# # # How to get the data type?
+# #     num = input()
+# #     if num.isdigit():
+# #         return "int"
+# #     elif num.replace(".", "").isdigit():
+# #         return "real"
+# #     elif num.isalpha():
+# #         return "string"
+# #     else:
+# #         return "unknown"
+
+# def multiply_by_two(num):
+#     return num * 2
+
+# def multiply_by_one_and_half(num):
+#     return num * 1.5
+
+# def surround_with_dollar_sign(num):
+#     return "$" + num + "$"
+
+# def print_result(result):
+#     print(result)
+
+# def main():
+#     data_type = get_data_type()
+#     num = input()
+#     if data_type == "int":
+#         result = multiply_by_two(int(num))
+#     elif data_type == "real":
+#         result = multiply_by_one_and_half(float(num))
+#     elif data_type == "string":
+#         result = surround_with_dollar_sign(num)
+#     print_result(result)
+
+# main()

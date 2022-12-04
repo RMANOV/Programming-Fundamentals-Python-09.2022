42cce2021746bd42a1318f6fb8c4ccfaf07512c7
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-12-04 11:47:29 +0200

    11.50

--- Easter Eggs - final exam1.py
+++ Easter Eggs - final exam1.py
@@ -18,9 +18,9 @@
 # You found 6 red eggs!
 # You found 5 purple eggs!
 
-import re
 
-initial_string = input()
+
+
 # match - colors -only lowercase letters,minimum 3 letters
 # match and numbers - amounts - only digits, minimum 1 digit
 # between the colors and numbers - any non alfabetical symbols and non digits
@@ -29,14 +29,40 @@ initial_string = input()
 # @@@@green@*/10/@yel0w@*26*#red#####//8//@limon*@*23*@@@red#*/%^&/6/@gree_een@/notnumber/###purple@@@@@*$%^&*/5/
 # #@##@red@#/8/@rEd@/2/#@purple@////10/
 
-patern = r"\b([a-z]{3,}+)\b([0-9]+)\b"
-valid_eggs_and_colors = re.finditer(patern, initial_string)
 
-for egg in valid_eggs_and_colors:
-    color = egg.group("color")
-    numbers = egg.group("numbers")
-    numbers = [ord(x) for x in numbers]
-    numbers = [str(x) for x in numbers]
-    numbers = " ".join(numbers)
-    print(f"You found {len(numbers)} {color} eggs!")
-    print(f"Your numbers are: {numbers}")
+import re
+
+
+initial_string = input()
+
+patern_colors = r"([a-z]{3,})+"
+patern_amounts = r"(\d{1,})"
+colors_and_amounts = {}
+
+for match in re.finditer(patern_colors, initial_string):
+    color = match.group()
+    if color not in colors_and_amounts:
+        colors_and_amounts[color] = 0
+    for match in re.finditer(patern_amounts, initial_string):
+        amount = match.group()
+        colors_and_amounts[color] += int(amount)
+        
+
+for color, amount in colors_and_amounts.items():
+    print(f"You found {amount} {color} eggs!")
+
+
+
+
+
+
+# valid_eggs_and_colors = re.finditer(patern, initial_string)
+
+# for egg in valid_eggs_and_colors:
+#     color = egg.group("color")
+#     numbers = egg.group("numbers")
+#     numbers = [ord(x) for x in numbers]
+#     numbers = [str(x) for x in numbers]
+#     numbers = " ".join(numbers)
+#     print(f"You found {len(numbers)} {color} eggs!")
+#     print(f"Your numbers are: {numbers}")

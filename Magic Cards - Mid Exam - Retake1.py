1c68a7e74894a5c8acc1e616cdab4e04140e1bfc
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-12-08 13:34:56 +0200

    13.35

--- Magic Cards - Mid Exam - Retake1.py
+++ Magic Cards - Mid Exam - Retake1.py
@@ -28,22 +28,24 @@ while True:
         else:
             new_deck.insert(int(command[2]), command[1])
     elif command[0] == "Remove":
-        if command[1] not in initial_deck:
+        if command[1] not in initial_deck or command[1] not in new_deck:
             print("Card not found.")
         else:
+            if command[1] in new_deck:
+                new_deck.remove(command[1])
+            else:
+                print("Card not found.")
+        if command[1] in new_deck:
             new_deck.remove(command[1])
     elif command[0] == "Swap":
-        if command[1] not in initial_deck or command[2] not in initial_deck:
-            print("Error!")
-        else:
-            first_card = new_deck.index(command[1])
-            second_card = new_deck.index(command[2])
-            new_deck[first_card], new_deck[second_card] = new_deck[second_card], new_deck[first_card]
+        first_card = new_deck.index(command[1])
+        second_card = new_deck.index(command[2])
+        new_deck[first_card], new_deck[second_card] = new_deck[second_card], new_deck[first_card]
     elif command[0] == "Shuffle":
         if new_deck:
             new_deck.reverse()
 print(" ".join(new_deck))
-    
+
 
 # initial_deck = input().split(":")
 # new_deck = []

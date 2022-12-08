0b31d889f1df553f21730fee16e3cb2fcb16f6e9
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-12-08 13:05:51 +0200

    13.05

--- Magic Cards - Mid Exam - Retake1.py
+++ Magic Cards - Mid Exam - Retake1.py
@@ -13,6 +13,7 @@
 # o	"Error!"
 
 initial_deck = input().split(":")
+new_deck = []
 
 while True:
     command = input().split()
@@ -22,25 +23,57 @@ while True:
         if command[1] not in initial_deck:
             print("Card not found.")
         else:
-            initial_deck.append(command[1])
+            new_deck.append(command[1])
     elif command[0] == "Insert":
         if command[1] not in initial_deck:
             print("Error!")
         else:
-            initial_deck.insert(int(command[2]), command[1])
+            new_deck.insert(int(command[2]), command[1])
     elif command[0] == "Remove":
         if command[1] not in initial_deck:
             print("Card not found.")
         else:
-            initial_deck.remove(command[1])
+            new_deck.remove(command[1])
     elif command[0] == "Swap":
         if command[1] not in initial_deck or command[2] not in initial_deck:
             print("Error!")
         else:
-            first_card = initial_deck.index(command[1])
-            second_card = initial_deck.index(command[2])
-            initial_deck[first_card], initial_deck[second_card] = initial_deck[second_card], initial_deck[first_card]
+            first_card = new_deck.index(command[1])
+            second_card = new_deck.index(command[2])
+            new_deck[first_card], new_deck[second_card] = new_deck[second_card], new_deck[first_card]
     elif command[0] == "Shuffle":
-        initial_deck.reverse()
+        new_deck.reverse()
 
-print(" ".join(initial_deck))
+print(" ".join(new_deck))
+
+# while True:
+#     command = input().split()
+#     if command[0] == "Ready":
+#         break
+#     elif command[0] == "Add":
+#         if command[1] not in initial_deck:
+#             print("Card not found.")
+#         else:
+#             initial_deck.remove(command[1])
+#             initial_deck.append(command[1])
+#     elif command[0] == "Insert":
+#         if command[1] not in initial_deck:
+#             print("Error!")
+#         else:
+#             initial_deck.insert(int(command[2]), command[1])
+#     elif command[0] == "Remove":
+#         if command[1] not in initial_deck:
+#             print("Card not found.")
+#         else:
+#             initial_deck.remove(command[1])
+#     elif command[0] == "Swap":
+#         if command[1] not in initial_deck or command[2] not in initial_deck:
+#             print("Error!")
+#         else:
+#             first_card = initial_deck.index(command[1])
+#             second_card = initial_deck.index(command[2])
+#             initial_deck[first_card], initial_deck[second_card] = initial_deck[second_card], initial_deck[first_card]
+#     elif command[0] == "Shuffle":
+#         initial_deck.reverse()
+
+# print(" ".join(initial_deck))
